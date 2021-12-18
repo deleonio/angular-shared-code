04cca6d302fb389933353e0543974ed6f033cfaa
Martin Oppitz <github@martinoppitz.com>
2021-12-18 13:34:01 +0100

    chore: initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BadgeComponent } from 'ng-lib/components/badge';
+import { BadgeInSrcComponent } from 'src/lib-copy/components/badge';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, BadgeComponent, BadgeInSrcComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
