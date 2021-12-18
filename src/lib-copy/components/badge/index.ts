e21ed8994ff0de855297ea612e5ac5d386eda5d1
Martin Oppitz <github@martinoppitz.com>
2021-12-18 13:34:54 +0100

    chore: add badge

---
+++ src/lib-copy/components/badge/index.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-badge-in-src',
+  templateUrl: './template.html',
+  styleUrls: ['./style.css']
+})
+export class BadgeInSrcComponent {
+  text = 'Ich bin ein Badge!';
+}
