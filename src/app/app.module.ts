f41fbf734e5669e672448ce465d77dfa30031960
Martin Oppitz <martin.oppitz@itzbund.de>
2021-12-24 14:00:08 +0100

    chore: runable variant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { BadgeComponent } from 'ng-lib/components/badge';
+// import { BadgeComponent } from 'ng-lib/components/badge';
 import { BadgeInSrcComponent } from 'src/lib-copy/components/badge';
 
 @NgModule({
-  declarations: [AppComponent, BadgeComponent, BadgeInSrcComponent],
+  declarations: [AppComponent, /* BadgeComponent,*/ BadgeInSrcComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
